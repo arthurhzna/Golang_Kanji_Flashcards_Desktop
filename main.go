5a93ab6378c537f2298f8e133d215a1934819392
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-15 11:31:04 +0700

    feat: update upload data file xlsx or xls feature

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
     "fmt"
-    "log"
 
     "github.com/xuri/excelize/v2"
     "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/app"
     "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/dialog"
+    "fyne.io/fyne/v2/storage"
     "fyne.io/fyne/v2/widget"
 )
 
@@ -15,17 +16,16 @@ type Kosakata struct {
     Kanji, Hiragana, Bacaan, Arti string
 }
 
-func main() {
-    // Load Excel
-    f, err := excelize.OpenFile("data.xlsx")
+func loadExcelData(filePath string) ([]Kosakata, error) {
+    f, err := excelize.OpenFile(filePath)
     if err != nil {
-        log.Fatal(err)
+        return nil, err
     }
     rows, _ := f.GetRows("Sheet1")
 
     var data []Kosakata
     for i, row := range rows {
-        if i == 0 {
+        if i == 0 { // skip header
             continue
         }
         if len(row) < 4 {
@@ -38,11 +38,10 @@ func main() {
             Arti:     row[3],
         })
     }
+    return data, nil
+}
 
-    a := app.New()
-    title := "Flashcard"
-    w := a.NewWindow(title)
-
+func createFlashcardUI(w fyne.Window, data []Kosakata) {
     index := 0
     showBacaan := true
     showArti := true
@@ -77,7 +76,6 @@ func main() {
             updateLabels()
         }
     })
-    // exitBtn removed
 
     toggleBacaanBtn := widget.NewButton("Hide", func() {
         showBacaan = !showBacaan
@@ -96,14 +94,75 @@ func main() {
             kanjiLabel,
             container.NewHBox(hiraLabel, toggleBacaanBtn),
             container.NewHBox(artiLabel, toggleArtiBtn),
-        ), 
-        container.NewGridWithColumns(2, prevBtn, nextBtn), 
-        nil, 
-        nil, 
-        nil, 
+        ), // top
+        container.NewGridWithColumns(2, prevBtn, nextBtn), // bottom
+        nil, // left
+        nil, // right
+        nil, // center
     )
 
     w.SetContent(content)
     w.Resize(fyne.NewSize(300, 90))
+}
+
+func main() {
+    a := app.New()
+    title := "Flashcard - Select Excel file"
+    w := a.NewWindow(title)
+
+    // Create a welcome message
+    welcomeLabel := widget.NewLabel("Select Excel file to load flashcard")
+    welcomeLabel.Alignment = fyne.TextAlignCenter
+
+    selectButton := widget.NewButton("Select Excel file", func() {
+        // Create file dialog
+        fileDialog := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+            if err != nil {
+                dialog.ShowError(err, w)
+                return
+            }
+            if reader == nil {
+                return // User cancelled
+            }
+            defer reader.Close()
+
+            filePath := reader.URI().Path()
+            
+            // Load data from selected file
+            data, loadErr := loadExcelData(filePath)
+            if loadErr != nil {
+                dialog.ShowError(fmt.Errorf("Error loading Excel file: %v", loadErr), w)
+                return
+            }
+
+            if len(data) == 0 {
+                dialog.ShowError(fmt.Errorf("Data format does not match"), w)
+                return
+            }
+
+            // Change window title and create flashcard UI
+            w.SetTitle("Flashcard")
+            createFlashcardUI(w, data)
+
+        }, w)
+
+        // Set file filter for Excel files
+        fileDialog.SetFilter(storage.NewExtensionFileFilter([]string{".xlsx", ".xls"}))
+        
+        // Set dialog size to be larger
+        fileDialog.Resize(fyne.NewSize(800, 600))
+        fileDialog.Show()
+    })
+
+    // Create initial content
+    content := container.NewVBox(
+        welcomeLabel,
+        widget.NewSeparator(),
+        selectButton,
+    )
+
+    w.SetContent(content)
+    // Increase main window size
+    w.Resize(fyne.NewSize(500, 250))
     w.ShowAndRun()
 }
